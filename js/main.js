17be0c4d76dfeae838a84c1dea549eb3a945e8ac
iazadur <iamazadur@gmail.com>
2021-08-16 09:24:04 +0600

    add

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+
+    // get user email 
+    const emailField = document.getElementById('user-email')
+    const userEmail = emailField.value
+    // get user password 
+    const passwordField = document.getElementById('user-password')
+    const userPassword = passwordField.value
+
+    if (userEmail == 'azad@gmail.com' && userPassword == 'asdf1010') {
+        location.href = 'banking.html'
+    } 
+
+})
+
+
